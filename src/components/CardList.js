a78260329c998507ed46d766801a75599bbb2cc1
Maxim Svishech <Maxim.Svishech@cellebrite.com>
2024-01-01 17:32:16 +0200

    fix scroll

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 const CardList = ({robots}) => {
   return (
-    <div style={{paddingBottom:'130px'}}>
+    <div>
       {robots.map((user, i) => {
         return (
           <Card
